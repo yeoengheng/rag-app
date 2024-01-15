69a340cd118f9218790378bfa3bcabcee3bd6e9c
Eng Heng <enghengyeo12@Hotmail.com>
2024-01-15 16:35:43 +0800

    added a few test routes

---
+++ app/api/retrieval/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+
---
+++ app/api/test/route.js
@@ -0,0 +1,46 @@
+import OpenAI from 'openai'
+import { NextResponse } from 'next/server'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+async function main(){
+    const completion = await openai.chat.completions.create({
+        messages:[{
+                role:"system",
+                content:"You are a helpful assistant designed to output JSON"
+            },
+            {
+                role:"user",
+                content:"give me a list of the presidents of Singapore"
+            }
+        ],
+        model:'gpt-3.5-turbo-1106',
+        response_format:{type: 'json_object'}
+    })
+    const myData = completion.choices[0].message.content
+    return myData
+}
+
+export async function GET(){
+    const res = await main()
+    return NextResponse.json({res})
+}
+
+
+// export async function GET(){
+//     const res = await fetch('https://cat-fact.herokuapp.com/facts',{
+//         headers:{
+//             'Content-Type': 'application/json',
+//         }
+//     })
+//     const catFact = await res.json()
+//     return Response.json({catFact})
+// }
+
+
+
+
+
+
