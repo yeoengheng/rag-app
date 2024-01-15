e0523a4e7ceb66575a662fc55281d8d8b98d3b8a
Eng Heng <enghengyeo12@Hotmail.com>
2024-01-15 17:00:54 +0800

    Configs

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-      <h2> Rag App here </h2>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-indigo-300">
+      <div className='flex justify-center items-center flex-col gap-4'>
+        <h2 className='text-3xl text-blue-900 font-bold'> RAG APP</h2>
         <Input></Input>
       </div>
     </main>
