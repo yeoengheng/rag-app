4b32773f8338c5da07401c5e7503d27fc25edb45
Eng Heng <enghengyeo12@Hotmail.com>
2024-01-17 11:48:25 +0800

    retrieval added

--- app/api/retrieval/chat/route.js
+++ app/api/retrieval/chat/route.js
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
+
 import { ChatOpenAI,OpenAIEmbeddings } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { SupabaseHybridSearch } from "@langchain/community/retrievers/supabase";
 
 export async function POST(req){
     try {
@@ -10,21 +14,46 @@ export async function POST(req){
             modelName: "gpt-3.5-turbo-1106",
             temperature: 0.7,
           });
-          const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_PUBLIC_KEY)
-          const vectorStore = await SupabaseVectorStore.fromTexts(
-            data,
-            new OpenAIEmbeddings(),
-            {
-              client,
-              tableName:'documents',
-              queryName:'match_documents'
-            }
+
+        const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.SUPABASE_PUBLIC_KEY)
+        const retriever = new SupabaseHybridSearch(
+          new OpenAIEmbeddings(),
+          {
+            client,
+            similarityK: 2,
+            keywordK: 2, 
+            tableName:"documents",
+            similarityQueryName:"match_documents",
+            keywordQueryName:"kw_match_documents",
+          }
           )
+        const addContext = await retriever.getRelevantDocuments(data)
 
-        
-    } catch (error) {
-        
+        const systemTemplate = "You are a Chinese Zodiac Fortune Teller."
+        const humanTemplate = `
+        Answer the user's question with some helpful context. Break down the context given.
+          <user>
+              {data}
+          </user> 
+          <context> 
+              {addContext}
+          </context>
+          `
+        const chatPrompt = ChatPromptTemplate.fromMessages([
+          ["system", systemTemplate],
+          ["human", humanTemplate],
+        ]);
+        const outputParser = new StringOutputParser()
+        const chain = chatPrompt.pipe(model).pipe(outputParser)
+        const results = await chain.invoke({
+          data:data,
+          addContext:addContext
+        })
+
+        return NextResponse.json({message:results},{status:200})
+    } 
+    catch (error) {
+      console.log(error)
+      return NextResponse.json({error:error},{status:500})
     }
-
-    return NextResponse.json({message:data},{status:200})
 }
